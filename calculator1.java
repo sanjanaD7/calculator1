06719556bdf272792566ea0dbdf1e52504ff2754
sanjana <01fe22bca054@kletech.ac.in>
2024-02-03 10:59:58 +0530

    calculator program

---
+++ calculator1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class calculator1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Sum: " + add(num1, num2));
+        System.out.println("Difference: " + subtract(num1, num2));
+        System.out.println("Product: " + multiply(num1, num2));
+        System.out.println("Quotient: " + divide(num1, num2));
+
+        scanner.close();
+    }
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+    public static double divide(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            System.out.println("Cannot divide by zero.");
+            return Double.NaN; 
+        }
+    }
+}
